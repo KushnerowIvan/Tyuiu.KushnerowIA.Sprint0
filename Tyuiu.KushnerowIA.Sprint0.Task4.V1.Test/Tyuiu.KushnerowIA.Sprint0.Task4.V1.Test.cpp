6625fa44d249d3485299bd4b93d4cc990a495c3a
tyuiu.pimenovaTS@outlook.com <Tatyana.filia.2005.pimenova@mail.ru>
2023-09-11 22:41:13 +0500

    Task4

---
+++ Tyuiu.KushnerowIA.Sprint0.Task4.V1.Test/Tyuiu.KushnerowIA.Sprint0.Task4.V1.Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuKushnerowIASprint0Task4V1Test
+{
+	TEST_CLASS(TyuiuKushnerowIASprint0Task4V1Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ Tyuiu.KushnerowIA.Sprint0.Task4.V1.lib/Tyuiu.KushnerowIA.Sprint0.Task4.V1.lib.cpp
@@ -0,0 +1,10 @@
+// Tyuiu.KushnerowIA.Sprint0.Task4.V1.lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnTyuiuKushnerowIASprint0Task4V1lib()
+{
+}
